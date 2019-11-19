bd20ccdb511593d044e91a45ed6243b15561ca60
David Dorothy <ddorothy@positiveaction.org>
2019-11-19 16:07:48 -0500

    Changes for release 0.0.3.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2019, David Dorothy'
 author = 'David Dorothy'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.3'
 
 
 # -- General configuration ---------------------------------------------------
