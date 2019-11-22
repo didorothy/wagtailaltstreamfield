f2f1f933b10e49c66e7bf13899b0e9a05505abea
David Dorothy <ddorothy@positiveaction.org>
2019-11-22 15:22:38 -0500

    Committed version 0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = '2019, David Dorothy'
 author = 'David Dorothy'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+release = '0.0.6'
 
 
 # -- General configuration ---------------------------------------------------
